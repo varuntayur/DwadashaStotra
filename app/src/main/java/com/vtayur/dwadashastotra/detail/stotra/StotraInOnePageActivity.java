93c9c4c82629f34c6bab503ef367a3e73c294855
varuntayur <varuntayur@gmail.com>
2014-10-03 12:22:17 +0530

    framework for learning mode - background related changes + all shlokas displayed in one page - done

--- app/src/main/java/com/vtayur/dwadashastotra/detail/stotra/StotraInOnePageActivity.java
+++ app/src/main/java/com/vtayur/dwadashastotra/detail/stotra/StotraInOnePageActivity.java
@@ -16,17 +16,14 @@
 
 package com.vtayur.dwadashastotra.detail.stotra;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -35,19 +32,10 @@ import com.vtayur.dwadashastotra.data.DataProvider;
 import com.vtayur.dwadashastotra.data.Language;
 import com.vtayur.dwadashastotra.data.model.Shloka;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-/**
- * Demonstrates a "screen-slide" animation using a {@link android.support.v4.view.ViewPager}. Because {@link android.support.v4.view.ViewPager}
- * automatically plays such an animation when calling {@link android.support.v4.view.ViewPager#setCurrentItem(int)}, there
- * isn't any animation-specific code in this sample.
- * <p/>
- * <p>This sample shows a "next" button that advances the user to the next step in a wizard,
- * animating the current screen out (to the left) and the next screen in (from the right). The
- * reverse animation is played when the user presses the "previous" button.</p>
- *
- * @see com.vtayur.dwadashastotra.detail.stotra.StotraPageFragment
- */
 public class StotraInOnePageActivity extends FragmentActivity {
 
     private static String TAG = "StotraInOnePageActivity";
@@ -60,45 +48,72 @@ public class StotraInOnePageActivity extends FragmentActivity {
 
         Log.d(TAG, "-> Starting StotraInOnePageActivity <-");
 
-        Typeface langTypeface = getTypeface();
+        Typeface typeface = getTypeface();
 
-        final LinearLayout lm = (LinearLayout) findViewById(R.id.rootLayout);
+        final LinearLayout rootLayout = (LinearLayout) findViewById(R.id.rootLayout);
 
-        // create the layout params that will be used to define how your
-        // button will be displayed
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        Integer menuPosition = getIntent().getIntExtra("menuPosition", 0);
+        rootLayout.setBackgroundResource(DataProvider.getBackgroundColor(menuPosition - 1));
 
-        // Create LinearLayout
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
+        TextView tvTitle = (TextView) findViewById(R.id.sectiontitle);
+        tvTitle.setText(getIntent().getStringExtra("sectionName"));
 
-        // Create TextView
-        TextView product = new TextView(this);
-        product.setText(" Product"+0+"    ");
-        ll.addView(product);
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        lm.addView(ll);
+        List<Shloka> engShlokas = (List<Shloka>) getIntent().getSerializableExtra("shlokaList");
+        List<Shloka> localLangShlokas = (List<Shloka>) getIntent().getSerializableExtra("shlokaListLocalLang");
 
-//        Integer menuPosition = getIntent().getIntExtra("menuPosition", 0);
-//        mPager.setBackgroundResource(DataProvider.getBackgroundColor(menuPosition - 1));
-//
-//        String mSectionName = getIntent().getStringExtra("sectionName");
-//        List<Shloka> engShlokas = (List<Shloka>) getIntent().getSerializableExtra("shlokaList");
-//
-//        List<Shloka> localLangShlokas = (List<Shloka>) getIntent().getSerializableExtra("shlokaListLocalLang");
-//
-//        PagerAdapter mPagerAdapter = new ShlokaSlidePagerAdapter(mSectionName, engShlokas, localLangShlokas, getFragmentManager(), langTypeface);
-//
-//        mPager.setAdapter(mPagerAdapter);
-//
-//        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                invalidateOptionsMenu();
-//            }
-//        });
+        Log.d(TAG, "StotraInOnePageActivity needs to render " + localLangShlokas.size() + " shlokas");
+        Log.d(TAG, "StotraInOnePageActivity needs to render english " + engShlokas.size() + " shlokas");
+
+        List<Pair<Shloka, Shloka>> lstPairShlokas = getListPairedShlokas(engShlokas, localLangShlokas);
+
+        for (Pair<Shloka, Shloka> shlokaPair : lstPairShlokas) {
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.VERTICAL);
+
+            TextView localLang = new TextView(this);
+            localLang.setTypeface(typeface);
+            localLang.setText(shlokaPair.second.getText());
+
+            TextView engLang = new TextView(this);
+            engLang.setText(shlokaPair.first.getText());
+
+            ll.addView(localLang);
+            ll.addView(engLang);
+
+            ll.setPadding(0, 5, 0, 50);
+            rootLayout.addView(ll);
+        }
+
         Log.d(TAG, "* StotraInOnePageActivity created *");
+    }
+
+    private List<Pair<Shloka, Shloka>> getListPairedShlokas(List<Shloka> engShlokas, List<Shloka> localLangShlokas) {
+        List<Pair<Shloka, Shloka>> lstPairShlokas = new ArrayList<Pair<Shloka, Shloka>>();
+
+        Iterator<Shloka> iterLocalLang = localLangShlokas.iterator();
+        for (Shloka shloka : engShlokas) {
+            if (iterLocalLang.hasNext()) {
+                Pair<Shloka, Shloka> pair = new Pair<Shloka, Shloka>(shloka, iterLocalLang.next());
+                lstPairShlokas.add(pair);
+            } else {
+                Pair<Shloka, Shloka> pair = new Pair<Shloka, Shloka>(shloka, new Shloka());
+                lstPairShlokas.add(pair);
+            }
+        }
+
+        if (engShlokas.size() < localLangShlokas.size()) {
+            Log.w(TAG, "getListPairedShlokas found a mismatch in eng vs. local lang.. " + engShlokas.size() + " vs. " + localLangShlokas.size() + " shlokas");
+            while (iterLocalLang.hasNext()) {
+                Pair<Shloka, Shloka> pair = new Pair<Shloka, Shloka>(new Shloka(), iterLocalLang.next());
+                lstPairShlokas.add(pair);
+            }
+
+        }
+
+        return lstPairShlokas;
     }
 
     @Override
@@ -132,35 +147,5 @@ public class StotraInOnePageActivity extends FragmentActivity {
     private String getSelectedLanguage() {
         SharedPreferences sharedPreferences = getSharedPreferences(DataProvider.PREFS_NAME, 0);
         return sharedPreferences.getString(DataProvider.SHLOKA_DISP_LANGUAGE, Language.san.toString());
-    }
-
-    /**
-     * A simple pager adapter that represents 5 {@link com.vtayur.dwadashastotra.detail.stotra.StotraPageFragment} objects, in
-     * sequence.
-     */
-    private class ShlokaSlidePagerAdapter extends FragmentStatePagerAdapter {
-
-        private final Typeface tf;
-        private final String sectionName;
-        private List<Shloka> shlokas;
-        private List<Shloka> localLangShlokas;
-
-        public ShlokaSlidePagerAdapter(String sectionName, List<Shloka> shlokas, List<Shloka> localizedShlokas, FragmentManager fm, Typeface tf) {
-            super(fm);
-            this.tf = tf;
-            this.shlokas = shlokas;
-            this.sectionName = sectionName;
-            this.localLangShlokas = localizedShlokas;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return new StotraPageFragment(sectionName, shlokas, localLangShlokas, position, tf);
-        }
-
-        @Override
-        public int getCount() {
-            return shlokas.size();
-        }
     }
 }
